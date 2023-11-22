a8f968bb5df634329391c77ee00dafffbc10da2e
Dresh13 <95115289+Dresh13@users.noreply.github.com>
2023-11-21 18:21:57 -0600

    for brennans review

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+print('\n', sys.path, '\n')
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "supercreative.settings")
