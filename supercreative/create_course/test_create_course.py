7336ded2fc3524bb3f8760a223526c30fc28448d
ekruse09 <elijahkruse9@gmail.com>
2023-11-25 21:51:26 -0600

    create course tests and attempted to implement create_courses. having issues with the test setup

--- supercreative/create_course/test_create_course.py
+++ supercreative/create_course/test_create_course.py
@@ -9,9 +9,24 @@ class LoginUnitTest(TestCase):
     role = None
 
     def setUp(self):
+
         # create a mock course
+        '''
         self.course = Course.objects.create(course_id=1, course_name="Intro to Software Engineering",
                                             course_description="stuff", course_code="COMPSCI-361")
+        '''
+
+        new_course = Course(
+            course_id=1,
+            course_name="Intro to Software Engineering",
+            course_description="stuff",
+            course_code="COMPSCI-361"
+        )
+
+        new_course.save()
+
+        self.course = new_course
+
         # create a mock user
         self.user = User.objects.create(user_id=1, email="test@example.com",
                                         password="password123",
@@ -19,10 +34,12 @@ class LoginUnitTest(TestCase):
                                         phone_number="1234567890", address="123 Main St")
 
     def test_correct_course(self):
+        # correctly create a course
         self.assertTrue(create_course(self.course.course_id, self.course.course_name, self.course.course_description,
                                       self.course.course_code, self.user.role),
                         "create_course did not return true when it should have.")
 
+        # get created course and check its values
         created_course = Course.objects.get(course_id=self.course.course_id)
 
         self.assertEqual(created_course.course_id, self.course.course_id, "create_course did not correctly set the "
@@ -39,6 +56,7 @@ class LoginUnitTest(TestCase):
                                                                               "course_code")
 
     def test_invalid_course_id(self):
+        # attempt to create a course with various invalid course ids
         self.assertFalse(create_course("wrong", self.course.course_name, self.course.course_description,
                                        self.course.course_code, self.user.role),
                          "create_course did not return false when it should have.")
@@ -55,10 +73,12 @@ class LoginUnitTest(TestCase):
                                        self.course.course_code, self.user.role),
                          "create_course did not return false when it should have.")
 
+        # make sure the invalid courses weren't created
         with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
             created_course = Course.objects.get(course_id=self.course.course_name)
 
     def test_invalid_course_name(self):
+        # attempt to create a course with various invalid course names
         self.assertFalse(create_course(self.course.course_id, 1, self.course.course_description,
                                        self.course.course_code, self.user.role),
                          "create_course did not return false when it should have.")
@@ -71,10 +91,12 @@ class LoginUnitTest(TestCase):
                                        self.course.course_code, self.user.role),
                          "create_course did not return false when it should have.")
 
+        # make sure the invalid courses weren't created
         with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
             created_course = Course.objects.get(course_id=self.course.course_id)
 
     def test_invalid_course_description(self):
+        # attempt to create a course with various invalid course descriptions
         self.assertFalse(create_course(self.course.course_id, self.course.course_name, 1,
                                        self.course.course_code, self.user.role),
                          "create_course did not return false when it should have.")
@@ -87,10 +109,12 @@ class LoginUnitTest(TestCase):
                                        self.course.course_code, self.user.role),
                          "create_course did not return false when it should have.")
 
+        # make sure the invalid courses weren't created
         with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
             created_course = Course.objects.get(course_id=self.course.course_id)
 
     def test_invalid_course_code(self):
+        # attempt to create a course with various invalid course codes
         self.assertFalse(create_course(self.course.course_id, self.course.course_name, self.course.course_description,
                                        1, self.user.role),
                          "create_course did not return false when it should have.")
@@ -103,10 +127,12 @@ class LoginUnitTest(TestCase):
                                        None, self.user.role),
                          "create_course did not return false when it should have.")
 
+        # make sure the invalid courses weren't created
         with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
             created_course = Course.objects.get(course_id=self.course.course_id)
 
     def test_invalid_role(self):
+        # attempt to create a course with invalid user role
         self.assertFalse(create_course(self.course.course_id, self.course.course_name, self.course.course_description,
                                        self.course.course_code, ""),
                          "create_course did not return false when it should have.")
@@ -119,13 +145,16 @@ class LoginUnitTest(TestCase):
                                        self.course.course_code, None),
                          "create_course did not return false when it should have.")
 
+        # make sure the invalid courses weren't created
         with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
             created_course = Course.objects.get(course_id=self.course.course_id)
 
     def test_duplicates(self):
+        # create a course
         create_course(self.course.course_id, self.course.course_name, self.course.course_description,
                       self.course.course_code, self.user.role)
 
+        # try to create additional courses with duplicate values
         self.assertFalse(create_course(self.course.course_id, "different course name", self.course.course_description,
                                        "different course code", self.user.role),
                          "create_course did not return false when it should have.")
@@ -138,6 +167,7 @@ class LoginUnitTest(TestCase):
                                        self.course.course_code, self.user.role),
                          "create_course did not return false when it should have.")
 
+        # make sure none of the courses with duplicate values were created
         with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
             created_course = Course.objects.get(course_name="different course name")
 
