b60e379152478b11539e111e4684f3f051552e9d
ekruse09 <elijahkruse9@gmail.com>
2023-11-26 20:06:24 -0600

    create course tests and attempted to implement create_courses. having issues with the test setup

--- supercreative/create_course/create_course.py
+++ supercreative/create_course/create_course.py
@@ -5,23 +5,23 @@ def create_course(id, name, description, code, role):
 
     # check to see if the id, name, description, code, and role are valid
     if not (isinstance(id, int) and id > 0):
-        return False, "Invalid Course ID"
+        return False
 
     if not (isinstance(name, str)):
-        return False, "Invalid Course Name"
+        return False
 
     if not isinstance(description, str):
-        return False, "Invalid Course Description"
+        return False
 
     if not (isinstance(code, str)):
-        return False, "Invalid Course Code"
+        return False
 
     if role != "administrator":
-        return False, "Insufficient privileges"
+        return False
 
     # check database for duplicate courses
     if Course.objects.filter(course_id=id).exists() or Course.objects.filter(course_name=name).exists() or Course.objects.filter(course_code=code).exists():
-        return False, "Duplicate values found"
+        return False
 
     # create the course
     new_course = Course(
@@ -32,27 +32,4 @@ def create_course(id, name, description, code, role):
     )
     new_course.save()
 
-    return True, "Course created successfully"
-
-'''
-Course.Create(p1, p2, p3, p4, p5, p6)
-Preconditions:
-CourseID is a non-null and unique integer
-Course Name is a non-null and unique string
-Course Description is a non-null string
-Course Code is a non-null, unique, and properly formatted string (i.e. COMPSCI-361)
-Active user role is Administrator
-Postconditions: 
-Return false if the CourseID, Course Name, or Course Code already exists in the Courses table or Archived Courses table and indicate duplicated value
-Return false if active user role is not Administrator
-Return true if input was valid, create an entry in the Courses table with the input information, and go to the Course Details Page for the new course.
-Side-effects: none
-p1: courseID input
-p2: courseName input
-p3: courseDescription input
-p4: courseCode input
-p5: role of active user
-p6: boolean output
-
-
-'''
+    return True
--- supercreative/create_course/test_create_course.py
+++ supercreative/create_course/test_create_course.py
@@ -1,31 +1,19 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.test import TestCase, Client
-from supercreative.models import (User, Course, Section)
+from django.test import TestCase
+from supercreative.models import (User, Course)
 from supercreative.create_course.create_course import create_course
 
 
-class LoginUnitTest(TestCase):
+class CreateCourseTest(TestCase):
     course = None
     role = None
 
     def setUp(self):
-
-        # create a mock course
-        '''
-        self.course = Course.objects.create(course_id=1, course_name="Intro to Software Engineering",
-                                            course_description="stuff", course_code="COMPSCI-361")
-        '''
-
-        new_course = Course(
-            course_id=1,
-            course_name="Intro to Software Engineering",
-            course_description="stuff",
-            course_code="COMPSCI-361"
-        )
-
-        new_course.save()
-
-        self.course = new_course
+        # variable values to use in the tests
+        self.course_id = 1
+        self.course_name = "Intro to Software Engineering"
+        self.course_description = "stuff"
+        self.course_code = "COMPSCI-361"
 
         # create a mock user
         self.user = User.objects.create(user_id=1, email="test@example.com",
@@ -35,144 +23,144 @@ class LoginUnitTest(TestCase):
 
     def test_correct_course(self):
         # correctly create a course
-        self.assertTrue(create_course(self.course.course_id, self.course.course_name, self.course.course_description,
-                                      self.course.course_code, self.user.role),
+        self.assertTrue(create_course(self.course_id, self.course_name, self.course_description,
+                                      self.course_code, self.user.role),
                         "create_course did not return true when it should have.")
 
         # get created course and check its values
-        created_course = Course.objects.get(course_id=self.course.course_id)
+        created_course = Course.objects.get(course_id=self.course_id)
 
-        self.assertEqual(created_course.course_id, self.course.course_id, "create_course did not correctly set the "
-                                                                          "course_id")
+        self.assertEqual(created_course.course_id, self.course_id, "create_course did not correctly set the "
+                                                                   "course_id")
 
-        self.assertEqual(created_course.course_name, self.course.course_name, "create_course did not correctly set the "
-                                                                              "course_name")
+        self.assertEqual(created_course.course_name, self.course_name, "create_course did not correctly set the "
+                                                                       "course_name")
 
-        self.assertEqual(created_course.course_description, self.course.course_description, "create_course did not "
-                                                                                            "correctly set the "
-                                                                                            "course_description")
+        self.assertEqual(created_course.course_description, self.course_description, "create_course did not "
+                                                                                     "correctly set the "
+                                                                                     "course_description")
 
-        self.assertEqual(created_course.course_code, self.course.course_code, "create_course did not correctly set the "
-                                                                              "course_code")
+        self.assertEqual(created_course.course_code, self.course_code, "create_course did not correctly set the "
+                                                                       "course_code")
 
     def test_invalid_course_id(self):
         # attempt to create a course with various invalid course ids
-        self.assertFalse(create_course("wrong", self.course.course_name, self.course.course_description,
-                                       self.course.course_code, self.user.role),
+        self.assertFalse(create_course("wrong", self.course_name, self.course_description,
+                                       self.course_code, self.user.role),
                          "create_course did not return false when it should have.")
 
-        self.assertFalse(create_course("1", self.course.course_name, self.course.course_description,
-                                       self.course.course_code, self.user.role),
+        self.assertFalse(create_course("1", self.course_name, self.course_description,
+                                       self.course_code, self.user.role),
                          "create_course did not return false when it should have.")
 
-        self.assertFalse(create_course(None, self.course.course_name, self.course.course_description,
-                                       self.course.course_code, self.user.role),
+        self.assertFalse(create_course(None, self.course_name, self.course_description,
+                                       self.course_code, self.user.role),
                          "create_course did not return false when it should have.")
 
-        self.assertFalse(create_course(-1, self.course.course_name, self.course.course_description,
-                                       self.course.course_code, self.user.role),
+        self.assertFalse(create_course(-1, self.course_name, self.course_description,
+                                       self.course_code, self.user.role),
                          "create_course did not return false when it should have.")
 
         # make sure the invalid courses weren't created
         with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
-            created_course = Course.objects.get(course_id=self.course.course_name)
+            Course.objects.get(course_name=self.course_name)
 
     def test_invalid_course_name(self):
         # attempt to create a course with various invalid course names
-        self.assertFalse(create_course(self.course.course_id, 1, self.course.course_description,
-                                       self.course.course_code, self.user.role),
+        self.assertFalse(create_course(self.course_id, 1, self.course_description,
+                                       self.course_code, self.user.role),
                          "create_course did not return false when it should have.")
 
-        self.assertFalse(create_course(self.course.course_id, True, self.course.course_description,
-                                       self.course.course_code, self.user.role),
+        self.assertFalse(create_course(self.course_id, True, self.course_description,
+                                       self.course_code, self.user.role),
                          "create_course did not return false when it should have.")
 
-        self.assertFalse(create_course(self.course.course_id, None, self.course.course_description,
-                                       self.course.course_code, self.user.role),
+        self.assertFalse(create_course(self.course_id, None, self.course_description,
+                                       self.course_code, self.user.role),
                          "create_course did not return false when it should have.")
 
         # make sure the invalid courses weren't created
         with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
-            created_course = Course.objects.get(course_id=self.course.course_id)
+            Course.objects.get(course_id=self.course_id)
 
     def test_invalid_course_description(self):
         # attempt to create a course with various invalid course descriptions
-        self.assertFalse(create_course(self.course.course_id, self.course.course_name, 1,
-                                       self.course.course_code, self.user.role),
+        self.assertFalse(create_course(self.course_id, self.course_name, 1,
+                                       self.course_code, self.user.role),
                          "create_course did not return false when it should have.")
 
-        self.assertFalse(create_course(self.course.course_id, self.course.course_name, False,
-                                       self.course.course_code, self.user.role),
+        self.assertFalse(create_course(self.course_id, self.course_name, False,
+                                       self.course_code, self.user.role),
                          "create_course did not return false when it should have.")
 
-        self.assertFalse(create_course(self.course.course_id, self.course.course_name, None,
-                                       self.course.course_code, self.user.role),
+        self.assertFalse(create_course(self.course_id, self.course_name, None,
+                                       self.course_code, self.user.role),
                          "create_course did not return false when it should have.")
 
         # make sure the invalid courses weren't created
         with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
-            created_course = Course.objects.get(course_id=self.course.course_id)
+            Course.objects.get(course_id=self.course_id)
 
     def test_invalid_course_code(self):
         # attempt to create a course with various invalid course codes
-        self.assertFalse(create_course(self.course.course_id, self.course.course_name, self.course.course_description,
+        self.assertFalse(create_course(self.course_id, self.course_name, self.course_description,
                                        1, self.user.role),
                          "create_course did not return false when it should have.")
 
-        self.assertFalse(create_course(self.course.course_id, self.course.course_name, self.course.course_description,
+        self.assertFalse(create_course(self.course_id, self.course_name, self.course_description,
                                        True, self.user.role),
                          "create_course did not return false when it should have.")
 
-        self.assertFalse(create_course(self.course.course_id, self.course.course_name, self.course.course_description,
+        self.assertFalse(create_course(self.course_id, self.course_name, self.course_description,
                                        None, self.user.role),
                          "create_course did not return false when it should have.")
 
         # make sure the invalid courses weren't created
         with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
-            created_course = Course.objects.get(course_id=self.course.course_id)
+            Course.objects.get(course_id=self.course_id)
 
     def test_invalid_role(self):
         # attempt to create a course with invalid user role
-        self.assertFalse(create_course(self.course.course_id, self.course.course_name, self.course.course_description,
-                                       self.course.course_code, ""),
+        self.assertFalse(create_course(self.course_id, self.course_name, self.course_description,
+                                       self.course_code, ""),
                          "create_course did not return false when it should have.")
 
-        self.assertFalse(create_course(self.course.course_id, self.course.course_name, self.course.course_description,
-                                       self.course.course_code, "student"),
+        self.assertFalse(create_course(self.course_id, self.course_name, self.course_description,
+                                       self.course_code, "student"),
                          "create_course did not return false when it should have.")
 
-        self.assertFalse(create_course(self.course.course_id, self.course.course_name, self.course.course_description,
-                                       self.course.course_code, None),
+        self.assertFalse(create_course(self.course_id, self.course_name, self.course_description,
+                                       self.course_code, None),
                          "create_course did not return false when it should have.")
 
         # make sure the invalid courses weren't created
         with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
-            created_course = Course.objects.get(course_id=self.course.course_id)
+            Course.objects.get(course_id=self.course_id)
 
     def test_duplicates(self):
         # create a course
-        create_course(self.course.course_id, self.course.course_name, self.course.course_description,
-                      self.course.course_code, self.user.role)
+        create_course(self.course_id, self.course_name, self.course_description,
+                      self.course_code, self.user.role)
 
         # try to create additional courses with duplicate values
-        self.assertFalse(create_course(self.course.course_id, "different course name", self.course.course_description,
+        self.assertFalse(create_course(self.course_id, "different course name", self.course_description,
                                        "different course code", self.user.role),
                          "create_course did not return false when it should have.")
 
-        self.assertFalse(create_course(3, self.course.course_name, self.course.course_description,
+        self.assertFalse(create_course(3, self.course_name, self.course_description,
                                        "different course code", self.user.role),
                          "create_course did not return false when it should have.")
 
-        self.assertFalse(create_course(3, "different course name", self.course.course_description,
-                                       self.course.course_code, self.user.role),
+        self.assertFalse(create_course(3, "different course name", self.course_description,
+                                       self.course_code, self.user.role),
                          "create_course did not return false when it should have.")
 
         # make sure none of the courses with duplicate values were created
         with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
-            created_course = Course.objects.get(course_name="different course name")
+            Course.objects.get(course_name="different course name")
 
         with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
-            created_course = Course.objects.get(course_id=3)
+            Course.objects.get(course_id=3)
 
         with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
-            created_course = Course.objects.get(course_code="different course code")
+            Course.objects.get(course_code="different course code")
