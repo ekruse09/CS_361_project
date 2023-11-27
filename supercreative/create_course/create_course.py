1f5ace101a0f45a6648fc9e3d67224bfd0de9eb4
ekruse09 <elijahkruse9@gmail.com>
2023-11-26 20:12:21 -0600

    created tests for create_course and implemented create_course

--- supercreative/create_course/create_course.py
+++ supercreative/create_course/create_course.py
@@ -3,23 +3,27 @@ from supercreative.models import (Course, Section)
 
 def create_course(id, name, description, code, role):
 
-    # check to see if the id, name, description, code, and role are valid
+    # check for valid id
     if not (isinstance(id, int) and id > 0):
         return False
 
+    # check for valid name
     if not (isinstance(name, str)):
         return False
 
+    # check for valid description
     if not isinstance(description, str):
         return False
 
+    # check for valid course code (just checking for a unique string as of now)
     if not (isinstance(code, str)):
         return False
 
+    # checking for appropriate privileges
     if role != "administrator":
         return False
 
-    # check database for duplicate courses
+    # check database for duplicate ids, names, or course codes
     if Course.objects.filter(course_id=id).exists() or Course.objects.filter(course_name=name).exists() or Course.objects.filter(course_code=code).exists():
         return False
 
--- supercreative/create_course/test_create_course.py
+++ supercreative/create_course/test_create_course.py
@@ -14,17 +14,12 @@ class CreateCourseTest(TestCase):
         self.course_name = "Intro to Software Engineering"
         self.course_description = "stuff"
         self.course_code = "COMPSCI-361"
-
-        # create a mock user
-        self.user = User.objects.create(user_id=1, email="test@example.com",
-                                        password="password123",
-                                        role="administrator", first_name="John", last_name="Doe",
-                                        phone_number="1234567890", address="123 Main St")
+        self.role = "administrator"
 
     def test_correct_course(self):
         # correctly create a course
         self.assertTrue(create_course(self.course_id, self.course_name, self.course_description,
-                                      self.course_code, self.user.role),
+                                      self.course_code, self.role),
                         "create_course did not return true when it should have.")
 
         # get created course and check its values
@@ -46,19 +41,19 @@ class CreateCourseTest(TestCase):
     def test_invalid_course_id(self):
         # attempt to create a course with various invalid course ids
         self.assertFalse(create_course("wrong", self.course_name, self.course_description,
-                                       self.course_code, self.user.role),
+                                       self.course_code, self.role),
                          "create_course did not return false when it should have.")
 
         self.assertFalse(create_course("1", self.course_name, self.course_description,
-                                       self.course_code, self.user.role),
+                                       self.course_code, self.role),
                          "create_course did not return false when it should have.")
 
         self.assertFalse(create_course(None, self.course_name, self.course_description,
-                                       self.course_code, self.user.role),
+                                       self.course_code, self.role),
                          "create_course did not return false when it should have.")
 
         self.assertFalse(create_course(-1, self.course_name, self.course_description,
-                                       self.course_code, self.user.role),
+                                       self.course_code, self.role),
                          "create_course did not return false when it should have.")
 
         # make sure the invalid courses weren't created
@@ -68,15 +63,15 @@ class CreateCourseTest(TestCase):
     def test_invalid_course_name(self):
         # attempt to create a course with various invalid course names
         self.assertFalse(create_course(self.course_id, 1, self.course_description,
-                                       self.course_code, self.user.role),
+                                       self.course_code, self.role),
                          "create_course did not return false when it should have.")
 
         self.assertFalse(create_course(self.course_id, True, self.course_description,
-                                       self.course_code, self.user.role),
+                                       self.course_code, self.role),
                          "create_course did not return false when it should have.")
 
         self.assertFalse(create_course(self.course_id, None, self.course_description,
-                                       self.course_code, self.user.role),
+                                       self.course_code, self.role),
                          "create_course did not return false when it should have.")
 
         # make sure the invalid courses weren't created
@@ -86,15 +81,15 @@ class CreateCourseTest(TestCase):
     def test_invalid_course_description(self):
         # attempt to create a course with various invalid course descriptions
         self.assertFalse(create_course(self.course_id, self.course_name, 1,
-                                       self.course_code, self.user.role),
+                                       self.course_code, self.role),
                          "create_course did not return false when it should have.")
 
         self.assertFalse(create_course(self.course_id, self.course_name, False,
-                                       self.course_code, self.user.role),
+                                       self.course_code, self.role),
                          "create_course did not return false when it should have.")
 
         self.assertFalse(create_course(self.course_id, self.course_name, None,
-                                       self.course_code, self.user.role),
+                                       self.course_code, self.role),
                          "create_course did not return false when it should have.")
 
         # make sure the invalid courses weren't created
@@ -104,15 +99,15 @@ class CreateCourseTest(TestCase):
     def test_invalid_course_code(self):
         # attempt to create a course with various invalid course codes
         self.assertFalse(create_course(self.course_id, self.course_name, self.course_description,
-                                       1, self.user.role),
+                                       1, self.role),
                          "create_course did not return false when it should have.")
 
         self.assertFalse(create_course(self.course_id, self.course_name, self.course_description,
-                                       True, self.user.role),
+                                       True, self.role),
                          "create_course did not return false when it should have.")
 
         self.assertFalse(create_course(self.course_id, self.course_name, self.course_description,
-                                       None, self.user.role),
+                                       None, self.role),
                          "create_course did not return false when it should have.")
 
         # make sure the invalid courses weren't created
@@ -140,19 +135,19 @@ class CreateCourseTest(TestCase):
     def test_duplicates(self):
         # create a course
         create_course(self.course_id, self.course_name, self.course_description,
-                      self.course_code, self.user.role)
+                      self.course_code, self.role)
 
         # try to create additional courses with duplicate values
         self.assertFalse(create_course(self.course_id, "different course name", self.course_description,
-                                       "different course code", self.user.role),
+                                       "different course code", self.role),
                          "create_course did not return false when it should have.")
 
         self.assertFalse(create_course(3, self.course_name, self.course_description,
-                                       "different course code", self.user.role),
+                                       "different course code", self.role),
                          "create_course did not return false when it should have.")
 
         self.assertFalse(create_course(3, "different course name", self.course_description,
-                                       self.course_code, self.user.role),
+                                       self.course_code, self.role),
                          "create_course did not return false when it should have.")
 
         # make sure none of the courses with duplicate values were created
