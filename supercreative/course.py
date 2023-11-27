4d37fd5d0b91c896dde1a9211e1a5e10f9c6dc62
N7Ironclad <skinnyhan@gmail.com>
2023-11-27 16:40:59 -0600

    Tests and implementation for editing courses. Stub for deleting courses.

---
+++ supercreative/course.py
@@ -0,0 +1,31 @@
+from .models import Course, Section
+from django.core.exceptions import ObjectDoesNotExist
+'''
+course_id is immutable
+'''
+
+def edit_course(current_course_id, new_course_name='', new_course_description="", new_course_code=""):
+    course = Course.objects.get(course_id=current_course_id)
+    existing_course = None
+    try:
+        existing_course = Course.objects.get(course_name=new_course_name)
+    except ObjectDoesNotExist:
+        pass
+
+    try:
+        existing_course = Course.objects.get(course_code=new_course_code)
+    except ObjectDoesNotExist:
+        pass
+
+    if existing_course is not None:
+        return False
+
+    course.course_name = new_course_name if new_course_name != '' else course.course_name
+    course.course_description=new_course_description if new_course_description != '' else course.course_description
+    course.course_code=new_course_code if new_course_code != '' else course.course_code
+    course.save()
+    return True
+
+def delete_course(current_course_id):
+    # Can't delete course with sections
+    pass
---
+++ supercreative/tests/test_course_edit.py
@@ -0,0 +1,86 @@
+from django.test import TestCase, Client
+from supercreative.models import (User, Course, Section)
+from supercreative.course import edit_course
+
+class TestCourseEdit(TestCase):
+    base_course = None
+    updated_course = None
+    existing_course = None
+    def setUp(self):
+        self.base_course = Course.objects.create(course_id=101, course_name="Introduction to Python",
+                              course_description="A basic course on Python programming",
+                              course_code="PY101")
+        self.updated_course = {"course_name":"New Intro to Python",
+                               "course_description":"An up to date introductory course on Python",
+                               "good_course_code":"PY102", "bad_course_code":"PY151"}
+        self.existing_course = Course.objects.create(course_id=102, course_name="Intermediate Python Programming",
+                              course_description="A basic course on Python programming",
+                              course_code="PY151")
+
+    def test_successful_edit_all(self):
+        # Edit all
+        self.assertTrue(edit_course(self.base_course.course_id, self.updated_course["course_name"],
+                    self.updated_course["course_description"], self.updated_course["good_course_code"]))
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_name,
+                         self.updated_course["course_name"], "Course name change failed")
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_description,
+                         self.updated_course["course_description"], "Course description change failed")
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_code,
+                         self.updated_course["good_course_code"], "Course code change failed")
+
+    def test_successful_edit_name(self):
+        # Edit name only
+        self.assertTrue(edit_course(self.base_course.course_id, self.updated_course["course_name"], '', ''))
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_name,
+                         self.updated_course["course_name"], "Course name change failed")
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_description,
+                         self.base_course.course_description, "Course description changed")
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_code,
+                         self.base_course.course_code, "Course code changed")
+
+    def test_successful_edit_description(self):
+        # Edit description only
+        self.assertTrue(edit_course(self.base_course.course_id, '', self.updated_course["course_description"], ''))
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_name,
+                         self.base_course.course_name, "Course name changed")
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_description,
+                         self.updated_course["course_description"], "Course description change failed")
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_code,
+                         self.base_course.course_code, "Course code changed")
+
+    def test_successful_edit_code(self):
+        # Edit code only
+        self.assertTrue(edit_course(self.base_course.course_id, '', '', self.updated_course["good_course_code"]))
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_name,
+                         self.base_course.course_name, "Course name changed")
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_description,
+                         self.base_course.course_description, "Course description changed")
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_code,
+                         self.updated_course["good_course_code"], "Course code change failed")
+
+    def test_duplicates(self):
+        # Duplicate name
+        self.assertFalse(edit_course(self.base_course.course_id, self.existing_course.course_name, '', ''))
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_name,
+                         self.base_course.course_name, "Course name changed")
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_description,
+                         self.base_course.course_description, "Course description changed")
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_code,
+                         self.base_course.course_code, "Course code changed")
+
+        # Duplicate description allowed
+        self.assertTrue(edit_course(self.base_course.course_id, '', self.existing_course.course_description, ''))
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_name,
+                         self.base_course.course_name, "Course name changed")
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_description,
+                         self.existing_course.course_description, "Course description not changed")
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_code,
+                         self.base_course.course_code, "Course code changed")
+
+        self.assertFalse(edit_course(self.base_course.course_id, '', '', self.existing_course.course_code))
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_name,
+                         self.base_course.course_name, "Course name changed")
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_description,
+                         self.base_course.course_description, "Course description changed")
+        self.assertEqual(Course.objects.get(course_id=self.base_course.course_id).course_code,
+                         self.base_course.course_code, "Course code changed")
