9555709415b8ba41d572c7d2258bd21313c22b3c
Dresh13 <95115289+Dresh13@users.noreply.github.com>
2023-11-28 18:41:53 -0600

    renamed course correctly

--- supercreative/tests/test_course.py
+++ supercreative/tests/test_course.py
@@ -10,14 +10,14 @@ class CoursesViewTest(TestCase):
         Course.objects.create(course_id='1', course_name='Test Course', course_description='Test Description', course_code='TC101')
 
     def test_get_courses(self):
-        response = self.client.get('/courses/')  # Replace with your actual URL
+        response = self.client.get('/course/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'courses.html')
         self.assertIn('courses', response.context)
         self.assertTrue(response.context['pool'])
 
     def test_post_view_course(self):
-        response = self.client.post('/courses/', {'action': 'view_course', 'course_id': '1'})
+        response = self.client.post('/course/', {'action': 'view_course', 'course_id': '1'})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'course.html')
         self.assertIn('course', response.context)
@@ -25,7 +25,7 @@ class CoursesViewTest(TestCase):
         self.assertFalse(response.context['edit'])
 
     def test_post_request_edit(self):
-        response = self.client.post('/courses/', {'action': 'request_edit', 'course_id': '1'})
+        response = self.client.post('/course/', {'action': 'request_edit', 'course_id': '1'})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'course.html')
         self.assertIn('course', response.context)
@@ -33,28 +33,28 @@ class CoursesViewTest(TestCase):
         self.assertTrue(response.context['edit'])
 
     def test_post_request_new(self):
-        response = self.client.post('/courses/', {'action': 'request_new'})
+        response = self.client.post('/course/', {'action': 'request_new'})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'course.html')
         self.assertTrue(response.context['pool'])
         self.assertTrue(response.context['edit'])
 
     def test_post_new_course(self):
-        response = self.client.post('/courses/', {'action': 'new_course', 'course_id': '2', 'course_name': 'New Course', 'course_description': 'New Description', 'course_code': 'NC102'})
+        response = self.client.post('/course/', {'action': 'new_course', 'course_id': '2', 'course_name': 'New Course', 'course_description': 'New Description', 'course_code': 'NC102'})
         self.assertRedirects(response, '/course/', status_code=302, target_status_code=200)
         self.assertEqual(Course.objects.count(), 2)
 
     def test_post_edit_course(self):
-        response = self.client.post('/courses/', {'action': 'edit_course', 'course_id': '1', 'course_name': 'Updated Course', 'course_description': 'Updated Description', 'course_code': 'UC103'})
+        response = self.client.post('/course/', {'action': 'edit_course', 'course_id': '1', 'course_name': 'Updated Course', 'course_description': 'Updated Description', 'course_code': 'UC103'})
         self.assertRedirects(response, '/course/', status_code=302, target_status_code=200)
         updated_course = Course.objects.get(course_id='1')
         self.assertEqual(updated_course.course_name, 'Updated Course')
 
     def test_post_delete_course(self):
-        response = self.client.post('/courses/', {'action': 'delete_course', 'course_id': '1'})
+        response = self.client.post('/course/', {'action': 'delete_course', 'course_id': '1'})
         self.assertRedirects(response, '/course/', status_code=302, target_status_code=200)
         self.assertEqual(Course.objects.count(), 0)
 
     def test_post_invalid_action(self):
-        response = self.client.post('/courses/', {'action': 'invalid_action'})
+        response = self.client.post('/course/', {'action': 'invalid_action'})
         self.assertRedirects(response, '/course/', status_code=302, target_status_code=200)
