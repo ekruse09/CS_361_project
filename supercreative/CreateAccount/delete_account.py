8899d095c7b6f96b4255fdd34755b55c0a51eb7e
ekruse09 <elijahkruse9@gmail.com>
2023-11-29 16:11:08 -0600

    finished tests and implementation for delete_account.py.delete_user

---
+++ supercreative/CreateAccount/delete_account.py
@@ -0,0 +1,27 @@
+from supercreative.models import User, UserCourseAssignment
+from django.core.exceptions import ObjectDoesNotExist
+
+
+def delete_user(user_id, role):
+    # Preconditions
+    if role is None or user_id is None:
+        return False
+
+    if role.lower() != "administrator":
+        return False
+
+    try:
+        # Check if the target userID exists in the Users table
+        user = User.objects.get(user_id=user_id)
+    except ObjectDoesNotExist:
+        return False
+
+    # Postconditions
+    # Remove all entries from the User-Course Assignments table that have the deleted UserID
+    UserCourseAssignment.objects.filter(user_id=user).delete()
+
+    # Delete the selected User account
+    user.delete()
+
+    # Return true if the selected User account was deleted
+    return True
