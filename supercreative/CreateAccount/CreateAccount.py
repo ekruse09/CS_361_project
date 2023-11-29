b8912ad96ee5a723ee459cf06be341068b51e453
yazisparksyang <123493109+yazisparksyang@users.noreply.github.com>
2023-11-28 19:36:28 -0600

    updated

--- supercreative/CreateAccount/CreateAccount.py
+++ supercreative/CreateAccount/CreateAccount.py
@@ -3,10 +3,10 @@ from supercreative.models import User
 
 
 def create_user(uid, email, password, role, first, last, phone, address):
-    if not isinstance(uid, int) and User.objects.filter(user_id=uid).exists() or uid <= 0:
+    if not isinstance(uid, int) or User.objects.filter(user_id=uid).exists() or uid <= 0:
         return False
 
-    if not isinstance(email, str) and User.objects.filter(email=email).exists() or len(email) == 0:
+    if len(email) == 0 or (not isinstance(email, str) and User.objects.filter(email=email).exists()):
         return False
 
     hold = email
@@ -17,33 +17,47 @@ def create_user(uid, email, password, role, first, last, phone, address):
     else:
         return False
 
-    if not isinstance(password, str) or len(password) == 0:
+    if len(password) == 0 or not isinstance(password, str):
         return False
 
-    upper = any(i.isupper() for i in User.objects.get(user_id=1).password)
-    lower = any(i.islower() for i in User.objects.get(user_id=1).password)
-    special = any(i in string.punctuation for i in User.objects.get(user_id=1).password)
+    upper = any(i.isupper() for i in password)
+    lower = any(i.islower() for i in password)
+    special = any(i in string.punctuation for i in password)
 
     if not upper or not lower or not special:
         return False
 
-    if not isinstance(role, str) or len(role) == 0:
+    if len(role) == 0 or not isinstance(role, str):
         return False
 
-    if not isinstance(first, str) or len(first) == 0:
+    valid_roles = ["administrator", "Administrator", "TA", "Teaching Assistant", "teaching assistant", "instructor",
+                   "Instructor"]
+    if role not in valid_roles:
         return False
 
-    if not isinstance(last, str) or len(last) == 0:
+    if len(first) == 0 or not isinstance(first, str):
         return False
 
-    if not isinstance(phone, str) or len(phone) == 0:
+    if len(last) == 0 or not isinstance(last, str):
         return False
 
-    if not isinstance(address, str) or len(address) == 0:
+    if len(phone) == 0 or not isinstance(phone, str):
+        return False
+
+    clean_phone = ''
+
+    for char in phone:
+        if char.isdigit():
+            clean_phone += char
+
+    if len(clean_phone) < 10:
+        return False
+
+    if len(address) == 0 or not isinstance(address, str):
         return False
 
     new_user = User(user_id=uid, email=email, password=password, role=role, first_name=first, last_name=last,
-                    phone_number=phone, address=address)
+                    phone_number=clean_phone, address=address)
     new_user.save()
 
     return True
