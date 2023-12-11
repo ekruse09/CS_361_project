d73b54ddead135b4150c89667c81fbaf570717ad
yazisparksyang <123493109+yazisparksyang@users.noreply.github.com>
2023-12-11 02:07:49 -0600

    Delete Section

--- supercreative/delete_section/delete_section.py
+++ supercreative/delete_section/delete_section.py
@@ -1,11 +1,13 @@
 from supercreative.models import Section, UserCourseAssignment
-
+from django.core.exceptions import ObjectDoesNotExist
 
 def delete_section(delsection):
-    if not Section.objects.filter(delsection.section).exists():
-        return "Section does not exist"
-    if delsection in "":
+    if not delsection:
         return "No Section detected"
-    UserCourseAssignment.objects.filter(delsection.section).delete()
-    Section.objects.get(section_id=delsection).delete()
+    try:
+        section = Section.objects.get(section_id=delsection)
+    except ObjectDoesNotExist:
+        return "Section does not exist"
+    UserCourseAssignment.objects.filter(section_id=section).delete()
+    section.delete()
     return "Section deletion was successful"
--- supercreative/tests/test_delete_section.py
+++ supercreative/tests/test_delete_section.py
@@ -1,25 +1,31 @@
 from django.test import TestCase
-from supercreative.models import Section, UserCourseAssignment
+from supercreative.models import Section, UserCourseAssignment, Course, User
 from supercreative.delete_section.delete_section import delete_section
 
 
 class MyTestCase(TestCase):
     def setUp(self):
-        self.good_section = Section(section_id=1, course_id=1, section_type="LEC")
-        self.good_assign = UserCourseAssignment(user_id=1, section_id=1, course_id=1, section_type="LEC")
+        self.good_user = User(user_id=1, email="test@uwm.edu", password="test", role="Administrator", first_name="testfirst",
+                              last_name="testlast", phone_number="5555555555", address="123")
+        self.good_course = Course(course_id=1, course_name="test", course_description="test", course_code="test")
+        self.good_course.save()
+        self.good_section = Section(section_id=1, course_id=self.good_course, section_type="LEC")
+        self.good_section.save()
+        self.good_assign = UserCourseAssignment(user_id=self.good_user, section_id=self.good_section, course_id=self.good_course, section_type="LEC")
 
     def test_delete_section(self):
-        self.assertEqual(delete_section(self.good_section), "Section deletion was successful",
+
+        self.assertEqual(delete_section(self.good_section.section_id), "Section deletion was successful",
                          "Failed delete section")
         self.assertFalse(Section.objects.filter(section_id=1).exists(), "Failed section still exist")
-        self.assertFalse(UserCourseAssignment.section_id.objects.filter(self.good_section.section_id).exists(),
+        self.assertFalse(UserCourseAssignment.objects.filter(section_id=self.good_section).exists(),
                          "Section exist on User_Course_Assignment")
 
     def test_no_parameter(self):
         self.assertEqual(delete_section(""), "No Section detected", "Failed no section pass")
 
     def test_non_existing_section(self):
-        self.assertEqual(delete_section(Section(section_id=-1, course_id=1, section_type="LEC")),
-                         "Section deletion was successful", "Section_id is invalid")
-        self.assertEqual(delete_section(Section(section_id=2, course_id=1, section_type="LEC")),
+        self.assertEqual(delete_section(-1),
+                         "Section does not exist", "Section_id is invalid")
+        self.assertEqual(delete_section(2),
                          "Section does not exist", "Section does not exist")
