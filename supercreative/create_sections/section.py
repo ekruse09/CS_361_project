8b832590324b7ca980993720c0fe117157562017
ekruse09 <elijahkruse9@gmail.com>
2023-12-14 18:22:05 -0600

    reverted create section and section tests

--- supercreative/create_sections/section.py
+++ supercreative/create_sections/section.py
@@ -1,16 +1,9 @@
-from supercreative.models import Course, Section, User, UserCourseAssignment
+from supercreative.models import Course, Section, SectionType
 from django.core.exceptions import ObjectDoesNotExist
 
 
-def create_section(section_id, course, section_type, user):
+def create_section(course, section_type):
     # Preconditions
-
-    if not isinstance(user, User):
-        return "invalid input for user"
-
-    # Check if the SectionID is a valid int
-    if not isinstance(section_id, int) or section_id < 1:
-        return "sectionID must be an integer"
 
     # Check if the course param is of the correct type
     if not isinstance(course, Course):
@@ -22,33 +15,16 @@ def create_section(section_id, course, section_type, user):
     except ObjectDoesNotExist:
         return "course does not exist"
 
-    # Check if the SectionID already exists in the Sections table
-    if Section.objects.filter(section_id=section_id).exists():
-        return "section already exists"
-
-    # Check if Section Type is valid: "Lecture", "Lab", or "Discussion"
-    valid_section_types = ["LECTURE", "LAB", "DISCUSSION"]
-
-    if section_type is None or section_type.upper() not in valid_section_types:
+    if not SectionType.objects.filter(section_type_name=section_type).exists():
         return "section type is not valid"
 
     # Post conditions
     # Create an entry in the Sections table with the input information
     new_section = Section(
-        section_id=section_id,
         course_id=course,
-        section_type=section_type.lower()
+        section_type=SectionType.objects.get(section_type_name=section_type)
     )
     new_section.save()
-
-    new_user_course_assignment = UserCourseAssignment(
-        user_id=user,
-        section_id=new_section,
-        course_id=course,
-        section_type=section_type
-    )
-
-    new_user_course_assignment.save()
 
     # Return true if input was valid
     return "section was successfully created"
--- supercreative/tests/Unit_Tests/test_create_section.py
+++ supercreative/tests/Unit_Tests/test_create_section.py
@@ -1,117 +1,71 @@
-from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase
-from supercreative.models import Course, Section, User, UserCourseAssignment
+from supercreative.models import Course, Section, SectionType
 from supercreative.create_sections.section import create_section
 
 
 class CreateSectionTest(TestCase):
     course = None
+    course_two = None
+    section = None
 
     def setUp(self):
         # variable values to use in the tests
         self.section_id = 801
-        self.section_type = "lab"
+        self.section_type = SectionType.objects.create(section_type_name="lab")
 
         # set up a mock user
-        self.user = User.objects.create(user_id=1, email="test@example.com",
-                                        password="password123",
-                                        role="student", first_name="John", last_name="Doe",
-                                        phone_number="1234567890", address="123 Main St")
-
         # set up two mock courses
-        self.course = Course.objects.create(course_name="Intro to Software Engineering", course_id=1,
+        self.course = Course.objects.create(course_name="Intro to Software Engineering",
                                             course_description="stuff", course_code="COMPSCI-361")
 
-        self.course_two = Course.objects.create(course_name="System Programming", course_id=5,
+        self.course_two = Course.objects.create(course_name="System Programming",
                                                 course_description="stuff", course_code="COMPSCI-337")
 
         # set up a mock section (to test for duplicates)
-        self.section = Section.objects.create(section_id=803, course_id=self.course_two,
-                                              section_type="discussion")
+        self.section = Section.objects.create(course_id=self.course_two,
+                                              section_type=SectionType.objects.create(
+                                                  section_type_name="discussion").section_type_name)
 
     def test_correct_section(self):
-        # correctly create a section
-        self.assertEqual(
-            create_section(section_id=self.section_id, course=self.course, section_type=self.section_type, user=self.user),
-            "section was successfully created",
-            "create_section did return the correct message.")
-
-        # get created section and check its values
-        created_section = Section.objects.get(section_id=self.section_id)
-
-        self.assertEqual(created_section.section_id, self.section_id, "create_section did not correctly set the "
-                                                                      "section_id")
-
-        self.assertEqual(created_section.section_type, self.section_type.lower(), "create_course did not "
-                                                                          "correctly set the "
-                                                                          "section_type")
-
-        # check user assignment
-        user_assignment = UserCourseAssignment.objects.get(user_id=self.user, section_id=self.section_id, course_id=self.course, section_type=self.section_type.lower())
-        self.assertIsNotNone(user_assignment, "UserCourseAssignment was not created.")
+        self.assertEqual(create_section(self.course, self.section_type.section_type_name),
+                         "section was successfully created")
+        self.assertEqual(Section.objects.all().count(), 2, "create_course did not correctly create the section")
 
     # course must exist
     def test_invalid_course(self):
         # attempt to create a section with various invalid courses
-        self.assertEqual(create_section(self.section_id, 3465423, self.section_type, self.user),
+        self.assertEqual(create_section(3465423, self.section_type.section_type_name),
                          "invalid input for course",
                          "create_section did return the correct message.")
 
-        self.assertEqual(create_section(self.section_id, None, self.section_type, self.user),
+        self.assertEqual(create_section(None, self.section_type.section_type_name),
                          "invalid input for course",
                          "create_section did return the correct message.")
 
-        self.assertEqual(create_section(self.section_id, "wrong", self.section_type, self.user),
+        self.assertEqual(create_section("wrong", self.section_type.section_type_name),
                          "invalid input for course",
                          "create_section did return the correct message.")
 
-        self.assertEqual(create_section(self.section_id, -1, self.section_type, self.user),
+        self.assertEqual(create_section(-1, self.section_type.section_type_name),
                          "invalid input for course",
                          "create_section did return the correct message.")
 
         # make sure the invalid sections weren't created
-        with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
-            Section.objects.get(section_id=self.section_id)
+        self.assertEqual(Section.objects.all().count(), 1, "create_course created a section when it shouldn't have")
 
-    # section id must be a unique integer
-    def test_invalid_section_id(self):
-        # attempt to create a course with various invalid section ids
-        self.assertEqual(create_section(-1, self.course, self.section_type, self.user),
-                         "sectionID must be an integer",
-                         "create_section did return the correct message.")
-
-        self.assertEqual(create_section(None, self.course, self.section_type, self.user),
-                         "sectionID must be an integer",
-                         "create_section did return the correct message.")
-
-        self.assertEqual(create_section("wrong", self.course, self.section_type, self.user),
-                         "sectionID must be an integer",
-                         "create_section did return the correct message.")
-
-        # duplicate section_id
-        self.assertEqual(create_section(self.section.section_id, self.course, self.section_type, self.user),
-                         "section already exists",
-                         "create_section did return the correct message.")
-
-        # make sure the invalid sections weren't created
-        with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
-            Section.objects.get(course_id=self.course.course_id)
-
-    # section type must be: lecture, lab, or discussion
     def test_invalid_section_type(self):
         # attempt to create a course with various invalid section types
-        self.assertEqual(create_section(self.section_id, self.course, "wrong", self.user),
+        self.assertEqual(create_section(self.course, "DISCUSSION", ),
                          "section type is not valid",
                          "create_section did return the correct message.")
 
-        self.assertEqual(create_section(self.section_id, self.course, None, self.user),
+        self.assertEqual(create_section(self.course, None, ),
                          "section type is not valid",
                          "create_section did return the correct message.")
 
-        self.assertEqual(create_section(self.section_id, self.course, "", self.user),
+        self.assertEqual(create_section(self.course, "Lab", ),
                          "section type is not valid",
                          "create_section did return the correct message.")
 
         # make sure the invalid sections weren't created
-        with self.assertRaises(ObjectDoesNotExist, msg="an exception should've been raised here"):
-            Section.objects.get(section_id=self.section_id)
+        self.assertEqual(Section.objects.all().count(), 1, "create_course created a section when it shouldn't have")
