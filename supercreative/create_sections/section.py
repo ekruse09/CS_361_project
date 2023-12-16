cc3693aec6ab6886d5c86e755081cabe690ec6ef
ekruse09 <elijahkruse9@gmail.com>
2023-12-16 14:22:43 -0600

    modified assign_user and section.py

--- supercreative/create_sections/section.py
+++ supercreative/create_sections/section.py
@@ -1,4 +1,4 @@
-from supercreative.models import Course, Section, SectionType
+from supercreative.models import Course, Section, SectionType, UserCourseAssignment
 from django.core.exceptions import ObjectDoesNotExist
 
 
@@ -28,3 +28,15 @@ def create_section(course, section_type):
 
     # Return true if input was valid
     return "section was successfully created"
+
+
+def delete_section(delsection):
+    if not delsection:
+        return "No Section detected"
+    try:
+        section = Section.objects.get(section_id=delsection)
+    except ObjectDoesNotExist:
+        return "Section does not exist"
+    UserCourseAssignment.objects.filter(section_id=section).delete()
+    section.delete()
+    return "Section deletion was successful"
